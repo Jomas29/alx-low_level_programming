711a71057a54851848b2e067efdf4d2b548190cf
Mayowa <jomas_29@yahoo.com>
2023-06-15 16:32:46 -0700

    C programm to print size of types added

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,6 @@ int main(void)
 printf("size of a char: %zu byte(s)\n", sizeof(char));
 printf("size of an int: %zu byte(s)\n", sizeof(int));
 printf("size of a float: %zu byte(s)\n", sizeof(float));
-printf("size of a double: %zu byte(s)\n", sizeof(double));
 printf("size of a long int: %zu byte(s)\n", sizeof(long));
 printf("size of a long long int: %zu byte(s)\n", sizeof(long long));
 
