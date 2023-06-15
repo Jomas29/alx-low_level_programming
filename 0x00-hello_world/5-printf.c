d72014a4dde506b2b2626b360bbf19582df2cae6
Mayowa <jomas_29@yahoo.com>
2023-06-15 16:00:12 -0700

    C program using printf added

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Start of the program
+*
+* Description: This program prints the string adding a new line at the end
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
