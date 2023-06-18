6e93b7b0739bbdaf1600460ef6f33ad00bbdc2e0
Mayowa <jomas_29@yahoo.com>
2023-06-18 14:39:32 -0700

    Print all digits between 0 and 10 program added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Statr of the program
+*
+* Description: This program prints all single digit numbers of base 10
+*
+* Retunn: Always 0 (Success)
+*/
+
+int main(void)
+{
+int digit;
+for (digit = 0; digit <= 9; digit++)
+{
+putchar('0' +digit);
+}
+return (0);
+}
