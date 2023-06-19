d471fbc184197fec5346f09d4f9c77f7bf77132f
Mayowa <jomas_29@yahoo.com>
2023-06-19 04:50:28 -0700

    Print combination of 3 digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,18 +14,21 @@ int main(void)
 
 int b, c, d;
 
-for (b = 0; b <= 9; b++)
+for (b = 0; b <= 7; b++)
 {
 for (c = 1 + b; c <= 8; c++)
 {
-for (d = 1 + c; d <= 7; d++)
+for (d = 1 + c; d <= 9; d++)
 {
 putchar('0' + b);
 putchar('0' + c);
 putchar('0' + d);
 }
+if (b != 7 && c != 8 && d != 9)
+{
 putchar(',');
 putchar(' ');
+}
 }
 }
 putchar('\n');
