57e41b00d45b4b70081c66909ca93032f121279d
Mayowa <jomas_29@yahoo.com>
2023-06-19 04:09:32 -0700

    Print combination of digits program added

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,9 +10,11 @@
 
 int main(void)
 {
-for (int c = 0; c <= 8; c++)
+int c, d;
+
+for (c = 0; c <= 8; c++)
 {
-for (int d = c + 1; d <= 9; d++)
+for (d = c + 1; d <= 9; d++)
 {
 putchar('0' + c);
 putchar('0' + d);
