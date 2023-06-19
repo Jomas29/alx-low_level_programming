2f5c7e6a8c45cfe59c6a5acda94c09d43e881e26
Mayowa <jomas_29@yahoo.com>
2023-06-18 17:50:57 -0700

    Print number combinatios

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+*main - Start of the program
+*
+* Description: This program prints distinct combination of 2 digits
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int c = 0;
+for (c = 0; c <= 8; c++)
+{
+int d = 1;
+for (d = 1; d <= 9; d++)
+{
+putchar('0' + c);
+putchar('1' + d);
+if (c != 8 || d != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+return (0);
+}
