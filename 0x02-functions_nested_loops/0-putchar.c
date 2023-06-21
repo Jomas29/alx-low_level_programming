324eabc24881285561f5d996d2aa39e166998444
Mayowa <jomas_29@yahoo.com>
2023-06-21 14:03:49 -0700

    Print _putchar program added

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,24 @@
-#include "main.h"
+#include <stdio.h>
+
+/**
+* main - Start of the program
+*
+* Description: This program prints _putchar
+*
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+char alp[8] = "_putchar";
+int i;
+
+for (i = 0; i <= 7; i++)
+{
+putchar(alp[i]);
+}
+
+putchar('\n');
 
 return (0);
 }
