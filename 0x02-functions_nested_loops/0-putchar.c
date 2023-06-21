452e12ede2e4f734b4aaf0891f4f182eba60c274
Mayowa <jomas_29@yahoo.com>
2023-06-21 12:56:55 -0700

    Print _putchar program added

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,15 @@
-#include <stdio.h>
+#include "main.h"
 
-/**
-* main - Start of the program
-*
-* Description: This program prints _putchar
-*
-* Return: Always 0 (Success)
-*/
+int main(void) {
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
 
-int main(void)
-{
-char alp[8] = "_putchar";
-int i;
-
-for (i = 0; i <= 7; i++)
-{
-putchar(alp[i]);
-}
-
-putchar('\n');
-
-return (0);
+    return 0;
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,8 @@
-#include <unistd.h>
+#ifndef PUTCHAR_H
+#define PUTCHAR_H
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+/* Function prototype */
+int _putchar(char c);
+void print_alphabet(void);
+
+#endif /* PUTCHAR_H */
