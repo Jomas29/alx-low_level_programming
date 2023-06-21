efe625a7e8cba84a64c1c6460b5f3c2d14d968e7
Mayowa <jomas_29@yahoo.com>
2023-06-21 13:08:56 -0700

    Print _putchar program added

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,26 @@
-#ifndef PUTCHAR_H
-#define PUTCHAR_H
+#include <unistd.h>
 
-/* Function prototype */
-int _putchar(char c);
-void print_alphabet(void);
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
-#endif /* PUTCHAR_H */
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+
+	_putchar('\n');
+}
