41d9c50010508e2aa7f40da9cf366985bdf69089
Mayowa <jomas_29@yahoo.com>
2023-06-22 14:35:52 -0700

    Comment out error code

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,13 +10,13 @@ int main(void)
 int i;
 
 printf("Infinite loop incoming :(\n");
-
 i = 0;
 /**
-while (i < 10)
-{
-putchar(i);
-}
+* Comments out the code causing the loop
+* while (i < 10)
+* {
+* putchar(i);
+* }
 */
 printf("Infinite loop avoided! \\o/\n");
 
