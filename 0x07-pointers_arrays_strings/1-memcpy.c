22cdf335eb9e4f49bc68de5f9035a61984bba03c
Jomas <jomas_29@yahoo.com>
2023-07-03 14:47:04 -0700

    Copy memory area function added

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,12 +1,22 @@
-char *_memcpy(char *dest, char *src, unsigned int n) {
-    char *original_dest = dest;
+/**
+* _memcpy - Copies a memory area.
+* @dest: Pointer to the destination memory area.
+* @src: Pointer to the source memory area.
+* @n: Number of bytes to copy.
+*
+* Return: A pointer to the destination memory area @dest.
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+char *original_dest = dest;
 
-    while (n > 0) {
-        *dest = *src;
-        dest++;
-        src++;
-        n--;
-    }
+while (n > 0)
+{
+*dest = *src;
+dest++;
+src++;
+n--;
+}
 
-    return original_dest;
+return (original_dest);
 }
