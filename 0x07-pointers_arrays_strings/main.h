2e6ce6b00fe653b89549aa223d6724d3700298b3
Jomas <jomas_29@yahoo.com>
2023-07-03 13:48:15 -0700

    Locate character in string function

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#ifndef NULL
+#define NULL ((void *)0)
+#endif
+
 void *_memset(char *s, char b, unsigned int n);
 void simple_print_buffer(char *buffer, unsigned int size);
+char *_strchr(char *s, char c);
 
 #endif /* MAIN_H */
