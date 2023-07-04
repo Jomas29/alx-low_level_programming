51397958eba452d2a120d717e6e5cb4cc06364af
Jomas <jomas_29@yahoo.com>
2023-07-04 07:49:55 -0700

    Print ten times alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,15 +5,20 @@
 *
 * Return: None.
 */
+
 void print_alphabet_x10(void)
+
 {
 int i;
 char letter;
 
 for (i = 0; i < 10; i++)
 {
+
 letter = 'a';
+
 while (letter <= 'z')
+
 {
 _putchar(letter);
 letter++;
