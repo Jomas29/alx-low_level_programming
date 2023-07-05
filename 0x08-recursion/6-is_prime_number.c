6190d819d5afaff4d2ba06b88b830c2789f1e903
Jomas <jomas_29@yahoo.com>
2023-07-05 16:17:40 -0700

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,11 +3,9 @@
 /**
 * is_prime_number - Checks if a number is prime.
 * @n: The number to check.
-* @i: guesswd divisor
 *
 * Return: 1 if n is prime, 0 otherwise.
 */
-
 int is_prime_number(int n)
 {
 int i;
@@ -16,8 +14,10 @@ if (n <= 1)
 return (0);
 
 for (i = 2; i * i <= n; i++)
+{
 if (n % i == 0)
 return (0);
+}
 
 return (1);
 }
