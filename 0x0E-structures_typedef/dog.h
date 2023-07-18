1e35e53b1ca0b013d86e1f73fa4e4bcb657109c3
Jomas <jomas_29@yahoo.com>
2023-07-18 01:48:55 +0000

    Struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Structure representing a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: This structure represents a dog and
+ * stores its name, age, and owner.
+ */
+
 struct dog
 {
 char *name;
