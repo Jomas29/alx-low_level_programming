a15581fd2f15ae49886eb1444ed024750b33e115
Jomas <jomas_29@yahoo.com>
2023-07-21 19:50:08 -0700

    simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,35 +2,51 @@
 #include <stdlib.h>
 #include "3-calc.h"
 
+/**
+ * main - Entry point of the calculator program.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line arguments.
+ *
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-    int (*op_func)(int, int);
+int num1, num2, result;
+int (*op_func)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        return (98);
-    }
+/* Check if the number of command-line arguments is correct */
+if (argc != 4)
+{
+printf("Error\n");
+return (98);
+}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
-    op_func = get_op_func(argv[2]);
+/* Convert the input numbers to integers using atoi function */
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
 
-    if (op_func == NULL || argv[2][1] != '\0')
-    {
-        printf("Error\n");
-        return (99);
-    }
+/* Get the corresponding operator function using get_op_func */
+op_func = get_op_func(argv[2]);
 
-    if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
-    {
-        printf("Error\n");
-        return (100);
-    }
+/* Check if the operator is valid or if it's more than one character */
+if (op_func == NULL || argv[2][1] != '\0')
+{
+printf("Error\n");
+return (99);
+}
 
-    result = op_func(num1, num2);
-    printf("%d\n", result);
+/* Check if there is an attempt to divide by zero */
+if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+{
+printf("Error\n");
+return (100);
+}
 
-    return (0);
+/* Perform the operation using the selected operator function */
+result = op_func(num1, num2);
+
+/* Print the result */
+printf("%d\n", result);
+
+return (0);
 }
