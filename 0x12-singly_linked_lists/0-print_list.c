2bcce5fbdb32436a4c8a7bf42e3cc770f8722363
Jomas <jomas_29@yahoo.com>
2023-07-27 23:40:29 +0100

    Print list corrected

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,20 +9,20 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t node_count = 0;
+size_t node_count = 0;
 
-    while (h != NULL)
-    {
+while (h != NULL)
+{
         /* If str is NULL, print [0] (nil) */
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%d] %s\n", h->len, h->str);
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%d] %s\n", h->len, h->str);
 
-        h = h->next;
-        node_count++;
-    }
+h = h->next;
+node_count++;
+}
 
-    return (node_count);
+return (node_count);
 }
 
