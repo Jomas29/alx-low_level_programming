62a295ca41f0105c09ea12654efee57b33b067ee
Jomas <jomas_29@yahoo.com>
2023-08-04 02:31:00 +0100

    Assembly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,19 @@
 section .data
-hello_fmt db "Hello, Holberton", 0
-nl db 10, 0    ; New line character (ASCII value 10)
+	hello_fmt db "Hello, Holberton", 0
+	nl db 10, 0    ; New line character (ASCII value 10)
 
 section .text
-global _start
+	global main
 
-_start:
-; Call printf function
-mov rdi, hello_fmt
-call printf
+	extern printf
 
-; Exit the program
-mov rax, 60       ; System call number for exit
-xor rdi, rdi      ; Exit status 0
-syscall
+main:
+	; Call printf function
+	mov rdi, hello_fmt
+	call printf
+
+	; Exit the program
+	mov eax, 60       ; System call number for exit
+	xor edi, edi      ; Exit status 0
+	syscall
 
