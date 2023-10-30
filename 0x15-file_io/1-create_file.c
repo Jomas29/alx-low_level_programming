9db99d8df1c955445f50d53e309fec3e65ca3a5d
Jomas <jomas_29@yahoo.com>
2023-10-30 15:28:04 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
 /**
  * create_file - Creates a file and writes text content to it.
  * @filename: The name of the file to create.
- * @content: A NULL-terminated string to write to the file.
+ * @text_content: A NULL-terminated string to write to the file.
  *
  * Return: 1 on success, -1 on failure.
  */
@@ -18,12 +18,12 @@ int create_file(const char *filename, char *text_content)
 	int bytes_written;
 
 	if (filename == NULL)
-		return -1;
+		return (-1);
 
 	text_file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (text_file == -1)
-		return -1;
+		return (-1);
 
 	if (text_content != NULL)
 	{
@@ -32,11 +32,11 @@ int create_file(const char *filename, char *text_content)
 		if (bytes_written == -1)
 		{
 			close(text_file);
-			return -1;
+			return (-1);
 		}
 	}
 
 	close(text_file);
-	return 1;
+	return (1);
 }
 
